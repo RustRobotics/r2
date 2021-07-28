f23b4948e6aea77fd5c17491984dc0bbc3b99425
Xu Shaohua <shaohua@biofan.org>
2021-07-28 09:45:39 +0800

    rmw: Check double slashes

--- rmw/src/validate_topic_name.rs
+++ rmw/src/validate_topic_name.rs
@@ -23,6 +23,7 @@ pub enum TopicNameErrorType {
     ContainsRepeatedForwardSlash,
 
     /// Must not start with a number.
+    /// Slashes must not be followed with numbers.
     StartsWithNumber,
 
     /// Length of topic name is larger than `255 - 8`.
@@ -66,7 +67,7 @@ pub fn validate_topic_name(topic_name: &str) -> Result<(), TopicNameError> {
         return Err(TopicNameError::new(TopicNameErrorType::NotAbsolute, 0));
     }
 
-    // Catches both "/foo/" and "/"
+    // Catches both '/foo/' and '/'.
     if topic_name.ends_with('/') {
         return Err(TopicNameError::new(
             TopicNameErrorType::EndsWithForwardSlash,
@@ -84,7 +85,28 @@ pub fn validate_topic_name(topic_name: &str) -> Result<(), TopicNameError> {
         }
     }
 
-    // Check if the topic name is too long last, since it might be a soft invalidation
+    // Check for double '/' and tokens that start with a number.
+    let mut last_char_is_slash = false;
+    for (idx, c) in topic_name.chars().enumerate() {
+        if last_char_is_slash {
+            if c == '/' {
+                return Err(TopicNameError::new(
+                    TopicNameErrorType::ContainsRepeatedForwardSlash,
+                    idx,
+                ));
+            } else if c.is_digit(10) {
+                // This is the case where a '/' if followed by a number, i.e. [0-9].
+                return Err(TopicNameError::new(
+                    TopicNameErrorType::StartsWithNumber,
+                    idx,
+                ));
+            }
+        } else if c == '/' {
+            last_char_is_slash = true;
+        }
+    }
+
+    // Check if the topic name is too long last, since it might be a soft invalidation.
     if topic_name.len() > TOPIC_NAME_MAX_NAME_LENGTH {
         return Err(TopicNameError::new(
             TopicNameErrorType::TooLong,
