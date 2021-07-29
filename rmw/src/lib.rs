192b6c4a5474bc50381962311ad927504674c8dc
Xu Shaohua <shaohua@biofan.org>
2021-07-29 19:34:11 +0800

    rmw: Add incompatible qos enums

--- rmw/src/lib.rs
+++ rmw/src/lib.rs
@@ -2,17 +2,18 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
-mod domain_id;
-mod event;
-mod init;
-mod init_options;
-mod localhost;
-mod qos_policy_kind;
-mod qos_profiles;
-mod security_options;
-mod serialized_message;
-mod time;
-mod types;
-mod validate_namespace;
-mod validate_node_name;
-mod validate_topic_name;
+pub mod domain_id;
+pub mod event;
+pub mod events_statuses;
+pub mod init;
+pub mod init_options;
+pub mod localhost;
+pub mod qos_policy_kind;
+pub mod qos_profiles;
+pub mod security_options;
+pub mod serialized_message;
+pub mod time;
+pub mod types;
+pub mod validate_namespace;
+pub mod validate_node_name;
+pub mod validate_topic_name;
