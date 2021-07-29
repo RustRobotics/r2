a49a179ab02db34b3f22f3b1aed6e41a1ef79587
Xu Shaohua <shaohua@biofan.org>
2021-07-29 16:22:54 +0800

    rmw: Add constant qos profiles

--- rmw/src/qos_profiles.rs
+++ rmw/src/qos_profiles.rs
@@ -7,6 +7,9 @@ use std::time::Duration;
 use super::qos_policy_kind::{
     QoSDurabilityPolicy, QoSHistoryPolicy, QoSLivelinessPolicy, QoSReliabilityPolicy,
 };
+use super::time::duration_unspecified;
+
+pub const QOS_POLICY_DEPTH_SYSTEM_DEFAULT: usize = 0;
 
 /// R2 MiddleWare quality of service profile.
 #[derive(Debug)]
@@ -36,4 +39,112 @@ pub struct QoSProfile {
 
     /// If true, any R2 specific namespacing conventions will be circumvented.
     pub avoid_ros_namespace_conventions: bool,
+}
+
+impl QoSProfile {
+    #[inline]
+    pub fn sensor_data() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::KeepLast,
+            depth: 5,
+            reliability: QoSReliabilityPolicy::BestEffort,
+            durability: QoSDurabilityPolicy::Volatile,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+
+    #[inline]
+    pub fn parameters() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::KeepLast,
+            depth: 1000,
+            reliability: QoSReliabilityPolicy::Reliable,
+            durability: QoSDurabilityPolicy::Volatile,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+
+    #[inline]
+    pub fn services_default() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::KeepLast,
+            depth: 10,
+            reliability: QoSReliabilityPolicy::Reliable,
+            durability: QoSDurabilityPolicy::Volatile,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+
+    #[inline]
+    pub fn parameter_events() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::KeepLast,
+            depth: 1000,
+            reliability: QoSReliabilityPolicy::Reliable,
+            durability: QoSDurabilityPolicy::Volatile,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+
+    #[inline]
+    pub fn system_default() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::SystemDefault,
+            depth: QOS_POLICY_DEPTH_SYSTEM_DEFAULT,
+            reliability: QoSReliabilityPolicy::SystemDefault,
+            durability: QoSDurabilityPolicy::SystemDefault,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+
+    #[inline]
+    pub fn unknown() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::Unknown,
+            depth: QOS_POLICY_DEPTH_SYSTEM_DEFAULT,
+            reliability: QoSReliabilityPolicy::Unknown,
+            durability: QoSDurabilityPolicy::Unknown,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::Unknown,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
+}
+
+impl Default for QoSProfile {
+    fn default() -> Self {
+        QoSProfile {
+            history: QoSHistoryPolicy::KeepLast,
+            depth: 10,
+            reliability: QoSReliabilityPolicy::Reliable,
+            durability: QoSDurabilityPolicy::Volatile,
+            deadline: duration_unspecified(),
+            lifespan: duration_unspecified(),
+            liveliness: QoSLivelinessPolicy::SystemDefault,
+            liveliness_lease_duration: duration_unspecified(),
+            avoid_ros_namespace_conventions: false,
+        }
+    }
 }
