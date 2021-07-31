cc154c4ba2b4e823b382729f0d40c3ba18246224
Xu Shaohua <shaohua@biofan.org>
2021-07-31 10:31:24 +0800

    idl: Add nom deps

--- r2idl/src/lib.rs
+++ r2idl/src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
