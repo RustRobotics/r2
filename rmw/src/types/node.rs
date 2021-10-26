f6209352519802671bdcc8e9b5c7e4c585e06088
Xu Shaohua <shaohua@biofan.org>
2021-10-26 14:31:20 +0800

    rmw: Add get_all_topic_names_and_types to NodeTrait

--- rmw/src/types/node.rs
+++ rmw/src/types/node.rs
@@ -249,4 +249,52 @@ pub trait NodeTrait {
         &self,
         service_names_and_types: &mut NamesAndTypes,
     ) -> RetType;
+
+    /// Return all topic names and types in the ROS graph.
+    ///
+    /// This function returns an array of all topic names and types in the ROS graph
+    /// i.e. for which a publisher and/or a subscription exists, as discovered so far
+    /// by the given local node.
+    ///
+    /// Unless `no_demangle` is true, some demangling and filtering may take place when
+    /// listing topics as implemented by the middleware.
+    /// Whether demangling applies or not, and how it applies, depends on the underlying
+    /// implementation.
+    /// See http://design.ros2.org/articles/topic_and_service_names.html for an example
+    /// on how it is used in DDS and RTPS based implementations.
+    ///
+    /// Runtime behavior:
+    ///
+    /// To query the ROS graph is a synchronous operation.
+    /// It is also non-blocking, but it is not guaranteed to be lock-free.
+    /// Generally speaking, implementations may synchronize access to internal resources using
+    /// locks but are not allowed to wait for events with no guaranteed time bound (barring
+    /// the effects of starvation due to OS scheduling).
+    ///
+    /// Thread-safety:
+    ///
+    /// Nodes are thread-safe objects, and so are all operations on them except for finalization.
+    /// Therefore, it is safe to query the ROS graph using the same node concurrently.
+    /// However, when querying topic names and types:
+    /// - Access to the array of names and types is not synchronized.
+    /// It is not safe to read or write `topic_names_and_types`
+    /// while get_topic_names_and_types() uses it.
+    /// - The default allocators are thread-safe objects, but any custom `allocator` may not be.
+    /// Check your allocator documentation for further reference.
+    ///
+    /// Given `node` must be a valid node handle, as returned by create_node().
+    /// Given `topic_names_and_types` must be a zero-initialized array of names and types,
+    /// as returned by get_zero_initialized_names_and_types().
+    ///
+    /// Return `RET_OK` if the query was successful,
+    /// or return `RET_INVALID_ARGUMENT` if `topic_names_and_types` is NULL,
+    /// or return `RET_INVALID_ARGUMENT` if `topic_names_and_types` is not a zero-initialized array,
+    /// or return `RET_INCORRECT_RMW_IMPLEMENTATION` if the `node` implementation identifier
+    /// does not match this implementation,
+    /// or return `RET_ERROR` if an unspecified error occurs.
+    fn get_all_topic_names_and_types(
+        &self,
+        no_demangle: bool,
+        topic_names_and_types: &mut NamesAndTypes,
+    ) -> RetType;
 }
