fb60260d2715d978a8197589c5ddf76ad5f55852
Xu Shaohua <shaohua@biofan.org>
2021-10-26 11:23:57 +0800

    rmw: Init networl flow endpoint

--- rmw/src/network_flow_endpoint.rs
+++ rmw/src/network_flow_endpoint.rs
@@ -4,7 +4,7 @@
 
 /// Transport protocol types
 #[repr(u8)]
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum TransportProtocol {
     Unknown = 0,
     Udp,
@@ -14,7 +14,7 @@ pub enum TransportProtocol {
 
 /// Internet protocol types
 #[repr(u8)]
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum InternetProtocol {
     Unknown = 0,
     Ipv4,
@@ -22,12 +22,13 @@ pub enum InternetProtocol {
     Count,
 }
 
-/// Maximum length of internet address string including terminating null
+/// Maximum length of internet address string including terminating null.
+///
 /// Inspired from linux/inet.h
-pub const RMW_INET_ADDRSTRLEN: usize = 48;
+pub const INET_ADDRSTRLEN: usize = 48;
 
-/// Structure that describes network flow endpoint of a publisher or subscription
-#[derive(Debug)]
+/// Structure that describes network flow endpoint of a publisher or subscription.
+#[derive(Debug, Clone, PartialEq)]
 pub struct NetworkFlowEndpoint {
     /// Transport protocol
     pub transport_protocol: TransportProtocol,
@@ -45,5 +46,25 @@ pub struct NetworkFlowEndpoint {
     pub dscp: u8,
 
     /// Internet address
-    pub internet_address: [u8; RMW_INET_ADDRSTRLEN],
+    pub internet_address: [u8; INET_ADDRSTRLEN],
+}
+
+impl NetworkFlowEndpoint {
+    /// Return a NetworkFlowEndpoint struct with zero-initialized members.
+    pub fn zero_inialized() -> Self {
+        Self::default()
+    }
+}
+
+impl Default for NetworkFlowEndpoint {
+    fn default() -> Self {
+        Self {
+            transport_protocol: TransportProtocol::Unknown,
+            internet_protocol: InternetProtocol::Unknown,
+            transport_port: 0,
+            flow_label: 0,
+            dscp: 0,
+            internet_address: [0; INET_ADDRSTRLEN],
+        }
+    }
 }
