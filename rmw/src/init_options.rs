6193277347b36184cc803502cb3b56afeadcd01d
Xu Shaohua <shaohua@biofan.org>
2021-10-26 10:46:57 +0800

    rmw: Add InitOptionsTrait to init_options

--- rmw/src/init_options.rs
+++ rmw/src/init_options.rs
@@ -4,8 +4,9 @@
 
 use std::fmt;
 
-use crate::domain_id::DomainId;
+use crate::domain_id::{DomainId, DEFAULT_DOMAIN_ID};
 use crate::localhost::LocalhostOnly;
+use crate::ret_types::RetType;
 use crate::security_options::SecurityOptions;
 
 /// Options structure used during rmw_init().
@@ -34,11 +35,47 @@ pub struct InitOptions {
     pub enclave: String,
 
     /// Implementation defined init options.
-    // May be NULL if there are no implementation defined options.
-    pub imp: Box<dyn InitOptionsImpl>,
+    ///
+    /// May be NULL if there are no implementation defined options.
+    pub imp: Option<Box<dyn InitOptionsImpl>>,
 }
 
 /// Implementation defined options structure used during rmw_init().
 ///
 /// This should be defined by the rmw implementation.
 pub trait InitOptionsImpl: fmt::Debug {}
+
+impl InitOptions {
+    /// Return a zero initialized init options structure.
+    pub fn zero_initialized() -> Self {
+        Self::default()
+    }
+}
+
+impl Default for InitOptions {
+    fn default() -> Self {
+        Self {
+            domain_id: DEFAULT_DOMAIN_ID,
+            localhost_only: LocalhostOnly::UseDefault,
+            implementation_identifier: String::new(),
+            imp: None,
+            instance_id: 0,
+            enclave: String::new(),
+            security_options: SecurityOptions::default(),
+        }
+    }
+}
+
+pub trait InitOptionsTrait {
+    /// Initialize given init options with the default values and implementation specific values.
+    ///
+    /// The `impl` pointer should not be changed manually.
+    /// The given init options must be zero initialized.
+    /// If initialization fails, init options will remain zero initialized.
+    /// Giving an already initialized init options will result in a failure
+    /// with return code `RMW_RET_INVALID_ARGUMENT`.
+    fn init(&mut self) -> RetType;
+
+    /// Copy the given source init options to the destination init options.
+    fn copy(&self, dest: &mut Self) -> RetType;
+}
