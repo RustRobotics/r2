4d7f0e073ad66b58652a11267dc0aec0ca4e526a
Xu Shaohua <shaohua@biofan.org>
2021-10-26 10:43:29 +0800

    rmw: Remove RMW_ prefix from common ret types

--- rmw/src/ret_types.rs
+++ rmw/src/ret_types.rs
@@ -6,22 +6,22 @@
 pub type RetType = i32;
 
 /// The operation ran as expected
-pub const RMW_RET_OK: RetType = 0;
+pub const RET_OK: RetType = 0;
 /// Generic error to indicate operation could not complete successfully
-pub const RMW_RET_ERROR: RetType = 1;
+pub const RET_ERROR: RetType = 1;
 /// The operation was halted early because it exceeded its timeout critera
-pub const RMW_RET_TIMEOUT: RetType = 2;
+pub const RET_TIMEOUT: RetType = 2;
 /// The operation or event handling is not supported.
-pub const RMW_RET_UNSUPPORTED: RetType = 3;
+pub const RET_UNSUPPORTED: RetType = 3;
 
 /// Failed to allocate memory
-pub const RMW_RET_BAD_ALLOC: RetType = 10;
+pub const RET_BAD_ALLOC: RetType = 10;
 /// Argument to function was invalid
-pub const RMW_RET_INVALID_ARGUMENT: RetType = 11;
+pub const RET_INVALID_ARGUMENT: RetType = 11;
 /// Incorrect rmw implementation.
-pub const RMW_RET_INCORRECT_RMW_IMPLEMENTATION: RetType = 12;
+pub const RET_INCORRECT_RMW_IMPLEMENTATION: RetType = 12;
 
 // rmw node specific ret codes in 2XX
 /// Failed to find node name
 // Using same return code than in rcl
-pub const RMW_RET_NODE_NAME_NON_EXISTENT: RetType = 203;
+pub const RET_NODE_NAME_NON_EXISTENT: RetType = 203;
