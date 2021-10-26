467107fbbfc1d78fe994cb9fd3312a29888ed48e
Xu Shaohua <shaohua@biofan.org>
2021-10-26 14:44:47 +0800

    rmw: Simplify Message Sequence

--- rmw/src/message_sequence.rs
+++ rmw/src/message_sequence.rs
@@ -5,28 +5,39 @@
 use super::types::MessageInfo;
 
 /// Structure to hold a sequence of R2 messages.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct MessageSequence {
     /// Array of pointers to R2 messages.
     //void ** data;
-    pub data: *const u8,
+    pub data: Vec<usize>,
+    // The number of valid entries in `data`.
+    //pub size: usize,
 
-    /// The number of valid entries in `data`.
-    pub size: usize,
+    // The total allocated capacity of the data array.
+    //pub capacity: usize,
+}
 
-    /// The total allocated capacity of the data array.
-    pub capacity: usize,
+impl MessageSequence {
+    /// Return a MessageSequence struct with members initialized to `NULL`
+    pub fn zero_initialized() -> Self {
+        Self::default()
+    }
 }
 
 /// Structure to hold a sequence of message infos.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct MessageInfoSequence {
     /// Array of message info.
-    pub data: *const MessageInfo,
+    pub data: Vec<MessageInfo>,
+    // The number of valid entries in data.
+    //pub size: usize,
+    // The total allocated capacity of the data array.
+    //pub capacity: usize,
+}
 
-    /// The number of valid entries in data.
-    pub size: usize,
-
-    /// The total allocated capacity of the data array.
-    pub capacity: usize,
+impl MessageInfoSequence {
+    /// Return a MessageInfoSequence struct with members initialized to `NULL`
+    pub fn zero_initialized() -> Self {
+        Self::default()
+    }
 }
