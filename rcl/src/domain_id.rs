ab6001990aafa021d6a34fda00e6703388287316
Xu Shaohua <shaohua@biofan.org>
2024-02-06 16:14:31 +0800

    Remove rcl/

--- rcl/src/domain_id.rs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
-// in the LICENSE file.
-
-/// The default domain ID used by RCL.
-pub const DEFAULT_DOMAIN_ID: u64 = u64::MAX;
-
-pub const DOMAIN_ID_ENV_VAR: &str = "R2_DOMAIN_ID";
