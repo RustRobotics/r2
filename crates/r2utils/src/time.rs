08d2629e178015ade697b402428945eac929938f
Xu Shaohua <shaohua@biofan.org>
2024-02-23 09:29:13 +0800

    Remove r2utils

--- crates/r2utils/src/time.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by General Public License that can be found
-// in the LICENSE file.
-
-/// A single point in time, measured in nanoseconds since the Unix epoch.
-pub type TimePointValue = i64;
-
-/// A duration of time, measured in nanoseconds.
-pub type DurationValue = i64;
